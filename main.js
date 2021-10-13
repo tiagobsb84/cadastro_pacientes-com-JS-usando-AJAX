6428221148169bc763ae1f4c1cc1962a6fd3548f
Tiago Silva de Oliveira <tiagobsb84@gmail.com>
2021-10-12 23:10:13 -0300

    Criando formulario e deixando dinamico

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const pacientes = document.querySelectorAll(".paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-for (var i = 0; i <= pacientes.length; i++) {
+for (var i = 0; i < pacientes.length; i++) {
 
     var paciente = pacientes[i];
 
-    const tdAltura = paciente.querySelector(".info-altura");
-    const altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-    const tdPeso = paciente.querySelector(".info-peso");
-    const peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-    const tdImc = paciente.querySelector(".info-imc");
+    var tdImc = paciente.querySelector(".info-imc");
 
-    validPeso = true;
-    validAltura = true;
+    var validPeso = true;
+    var validAltura = true;
 
     if (peso <= 0 || peso >= 1000) {
         validPeso = false;
@@ -26,11 +26,46 @@ for (var i = 0; i <= pacientes.length; i++) {
         paciente.classList.add('paciente-invalido');
     }
     if (validPeso && validAltura) {
-        const imc = peso / (altura * altura);
+        var imc = peso / (altura * altura);
         tdImc.textContent = imc.toFixed(2);
     }
 }
 
+var addPacientes = document.querySelector("#addPaciente");
+
+addPacientes.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    var form = document.querySelector(".formAdicionar");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var trTabela = document.createElement('tr');
+
+    var tdNome = document.createElement('td');
+    var tdPeso = document.createElement('td');
+    var tdAltura = document.createElement('td');
+    var tdGordura = document.createElement('td');
+    var tdImc = document.createElement('td');
+
+    tdNome.textContent = nome;
+    tdPeso.textContent = peso;
+    tdAltura.textContent = altura;
+    tdGordura.textContent = gordura;
+
+    trTabela.appendChild(tdNome);
+    trTabela.appendChild(tdPeso);
+    trTabela.appendChild(tdAltura);
+    trTabela.appendChild(tdGordura);
+    trTabela.appendChild(tdImc);
+
+    var tabela = document.querySelector("#tabela-pacientes")
+
+    tabela.appendChild(trTabela);
+});
 
 
 
